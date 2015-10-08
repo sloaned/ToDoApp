a9151e99da8fbfc3906968590ae642a37503b14e
unknown <dsloane@PC30120.catalystsolves.com>
2015-10-08 15:10:52 -0700

    made toDoItem class

--- src/catalyst/application/ToDoItem.java
+++ src/catalyst/application/ToDoItem.java
@@ -1,5 +1,42 @@
 package catalyst.application;
 
-public class ToDoItem {
+import java.util.Date;
 
+public class ToDoItem {
+	private String task;
+	private boolean complete;
+	private boolean inProgress;
+	private String assignedUser;
+	private Date dueDate;
+	
+	public String getTask() {
+		return task;
+	}
+	public void setTask(String task) {
+		this.task = task;
+	}
+	public boolean isComplete() {
+		return complete;
+	}
+	public void setComplete(boolean complete) {
+		this.complete = complete;
+	}
+	public boolean isInProgress() {
+		return inProgress;
+	}
+	public void setInProgress(boolean inProgress) {
+		this.inProgress = inProgress;
+	}
+	public String getAssignedUser() {
+		return assignedUser;
+	}
+	public void setAssignedUser(String assignedUser) {
+		this.assignedUser = assignedUser;
+	}
+	public Date getDueDate() {
+		return dueDate;
+	}
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
 }
