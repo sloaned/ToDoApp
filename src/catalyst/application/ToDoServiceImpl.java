0ab511cf6ce4281d11545e449e0e6decd44dcedf
unknown <arecore@PC10328.catalystsolves.com>
2015-10-09 11:48:31 -0700

    menu is working

--- src/catalyst/application/ToDoServiceImpl.java
+++ src/catalyst/application/ToDoServiceImpl.java
@@ -7,6 +7,7 @@ import catalyst.data.ToDoData;
 
 public class ToDoServiceImpl implements ToDoService {
 
+	// Dependency to ToDoData
 	private ToDoData toDoData;
 	public void add(String task, boolean inProgress, String assignedUser, Date dueDate){
 		
--- src/catalyst/applicationRunner/Main.java
+++ src/catalyst/applicationRunner/Main.java
@@ -1,5 +1,7 @@
 package catalyst.applicationRunner;
 
+import catalyst.application.ToDoServiceImpl;
+import catalyst.data.ToDoArrayList;
 import catalyst.presentation.ToDoApp;
 
 public class Main {
@@ -7,18 +9,22 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		ToDoApp toDoApp = new ToDoApp();
+		ToDoArrayList toDoData = new ToDoArrayList();
+		ToDoServiceImpl toDoService = new ToDoServiceImpl();
+		toDoApp.setToDoService(toDoService);
+		
 		
 		toDoApp.displayMenu();
 		int userInput = toDoApp.getInput();
 		
 		while (userInput != 9)
 		{
-		     toDoApp.userChoice(toDoApp.getInput());
+		     toDoApp.userChoice(userInput);
 		     toDoApp.displayMenu();
-		     
 		}
 		
 		System.exit(0);	
 	}
 
 }
+
