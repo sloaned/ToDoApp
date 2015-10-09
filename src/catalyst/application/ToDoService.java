cc28b5d03015905d7f60a7a9741500ebf2008f98
unknown <dsloane@PC30120.catalystsolves.com>
2015-10-09 10:52:06 -0700

    wrote a bunch of update methods

--- src/catalyst/application/ToDoService.java
+++ src/catalyst/application/ToDoService.java
@@ -1,6 +1,16 @@
 package catalyst.application;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public interface ToDoService {
-	
+		
+	void add(String task, boolean inProgress, String assignedUser, Date dueDate);
+	void remove(int index);
+	ArrayList<ToDoItem> getAll();
+	void markComplete(int index);
+	void markIncomplete(int index);
+	ArrayList<ToDoItem> getComplete();
+	ArrayList<ToDoItem> getIncomplete();
 	
 }
