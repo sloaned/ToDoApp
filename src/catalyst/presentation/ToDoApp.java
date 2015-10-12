004f6bcfbc5771efa682d3501f48cca67db0a6cc
unknown <dsloane@PC30120.catalystsolves.com>
2015-10-12 11:10:02 -0700

    fixed show by user

--- src/catalyst/presentation/ToDoApp.java
+++ src/catalyst/presentation/ToDoApp.java
@@ -107,7 +107,7 @@ public class ToDoApp
 			newUser = scan.nextLine();
 		}
 		
-		System.out.println("Did you want to add a description about this task? (yes or no");
+		System.out.println("Did you want to add a description about this task? (yes or no)");
 		getAnswer = scan.nextLine();
 		if(getAnswer == "no" || getAnswer == "n")
 		{
@@ -236,7 +236,9 @@ public class ToDoApp
 		System.out.println("Input the name of a user you want to find the tasks for: ");
 		String getName = scan.nextLine();
 		
-		toDoService.getUserTask(getName);
+		ArrayList<ToDoItem> theList = toDoService.getUserTask(getName);
+		showList(theList);
+		
 	}
 	
 	public void removeListItem()
