4b30c73b10b4759dad3e398b26a2d8e4417767b6
unknown <arecore@PC10328.catalystsolves.com>
2015-10-12 10:39:34 -0700

    string stuff

--- src/catalyst/presentation/ToDoApp.java
+++ src/catalyst/presentation/ToDoApp.java
@@ -41,7 +41,306 @@ public class ToDoApp
 		System.out.println("12) Ok, I'm finished here");
 		System.out.println("");
 	}
+
+	public void getNewTask()
+	{
+		String newTask = null;
+		String getDate = null;
+		boolean inProgress = false;
+		String newUser = null;
+		String rawInput = null;
+		String getAnswer = null;
+		String newDescription = null;
+		
+		
+		System.out.println("Enter a new task: ");
+		newTask = scan.nextLine();
+		
+		while(newTask.length() < 1 || newTask.equals(" "))
+		{
+			System.out.println("Enter a new task: ");
+			newTask = scan.nextLine();
+		}
+		while(toDoService.inList(newTask) == true)
+		{
+			System.out.println("That task already exists, please enter a unique task name.");
+			newTask = scan.nextLine();
+		}
+		
+		
+		System.out.println("Enter the Due Date: ");
+		getDate = scan.nextLine();
+		while(!isDate(getDate))
+		{
+			System.out.println("Not a valid date");
+			getDate = scan.nextLine();
+		}
+		Date newDate = new Date ();
+		String pattern = "MM/dd/yyyy";
+	    SimpleDateFormat format = new SimpleDateFormat(pattern);
+		 
+	    try 
+	    {
+	      newDate = format.parse(getDate);
+	    } 
+	    catch (ParseException e) 
+	    {
+	      e.printStackTrace();
+	    }
+		
+	    System.out.println("Is this task in progress? (yes or no): ");
+		rawInput = scan.nextLine();
+		if (rawInput.equals("yes") || rawInput.equals("y"))
+		{
+			inProgress = true;
+		}
+		else 
+		{
+			inProgress = false;
+		}
+		
+		System.out.println("Who has to finish this task?: ");
+		newUser = scan.nextLine();
+		while(newUser.length() < 1 || newUser.equals(" "))
+		{
+			System.out.println("Enter a new task: ");
+			newUser = scan.nextLine();
+		}
+		
+		System.out.println("Did you want to add a description about this task? (yes or no");
+		getAnswer = scan.nextLine();
+		if(getAnswer == "no" || getAnswer == "n")
+		{
+			newDescription = null;
+		}
+		else if(getAnswer == "yes" || getAnswer == "y")
+		{
+			System.out.println("Please enter a description: ");
+			newDescription = scan.nextLine();
+		}
+		
+		toDoService.add(newTask, inProgress, newUser, newDate, newDescription);
+		
+	}
+	
+	public void updateTask()
+	{
+		getList();
+		String userEntry = getStringInput();
+		
+		String updateTask = null;
+		String getDate = null;
+		boolean inProgress = false;
+		String updateUser = null;
+		boolean isComplete = false;
+		String updateDescription = null;
+		String getAnswer = null;
+		
+		
+		System.out.println("Update the task: ");
+		updateTask = scan.nextLine();
+		
+		
+		System.out.println("Enter the new Due Date: ");
+		getDate = scan.nextLine();
+		Date updateDate = new Date ();
+		String pattern = "MM/dd/yyyy";
+	    SimpleDateFormat format = new SimpleDateFormat(pattern);
+	    try 
+	    {
+	      updateDate = format.parse(getDate);
+	    } 
+	    catch (ParseException e) 
+	    {
+	      e.printStackTrace();
+	    }
+		
+	    System.out.println("Is this task in progress? (yes or no): ");
+		String rawInput = scan.nextLine();
+		
+		if (rawInput == "yes"|| rawInput == "y")
+		{
+			inProgress = true;
 			
+		}
+		else if (rawInput.equals("no") || rawInput.equals("n"))
+		{
+			System.out.println("Is this task complete?");
+			String nextRawInput = scan.nextLine();
+			
+			if (nextRawInput.equals("yes") || nextRawInput.equals("y"))
+			{
+				isComplete = true;
+			}
+			else if (nextRawInput.equals("no") || nextRawInput.equals("n"))
+			{
+				isComplete = false;
+			}
+			
+			if (isComplete == false)
+			{
+				System.out.println("Who has to finish this task?: ");
+				updateUser = scan.nextLine();
+				while(updateUser.length() < 1 || updateUser.equals(" "))
+				{
+					System.out.println("Enter a new task: ");
+					updateUser = scan.nextLine();
+				}
+				
+			}
+			else if (isComplete == true)
+			{
+				System.out.println("Who finished this task?: ");
+				updateUser = scan.nextLine();
+				while(updateUser.length() < 1 || updateUser.equals(" "))
+				{
+					System.out.println("Enter a new task: ");
+					updateUser = scan.nextLine();
+				}
+			}
+			inProgress = false;
+		}
+		System.out.println("Did you want to add a description about this task? (yes or no");
+		getAnswer = scan.nextLine();
+		if(getAnswer == "no" || getAnswer == "n")
+		{
+			updateDescription = null;
+		}
+		else if(getAnswer == "yes" || getAnswer == "y")
+		{
+			System.out.println("Please enter a description: ");
+			updateDescription = scan.nextLine();
+		}
+		
+		toDoService.update(userEntry, updateTask, isComplete, inProgress, updateUser, updateDate, updateDescription);
+	}
+	
+	public boolean isDate(String input)
+	{
+		Date newDate = new Date ();
+		String pattern = "MM/dd/yyyy";
+	    SimpleDateFormat format = new SimpleDateFormat(pattern);
+		 try 
+		    {
+		      newDate = format.parse(input);
+		      return true;
+		    } 
+		    catch (ParseException e) 
+		    {
+		      return false;
+		    }
+	}
+	
+	public void selectUserTasks()
+	{
+		System.out.println("Input the name of a user you want to find the tasks for: ");
+		String getName = scan.nextLine();
+		
+		toDoService.getUserTask(getName);
+	}
+	
+	public void removeListItem()
+	{
+		getList();
+		
+		int rawInput = getInput();
+		ArrayList<ToDoItem> toDo = toDoService.getAll();
+		
+		
+		while(rawInput > toDo.size() || rawInput < 0)
+		{
+			System.out.println("That is not an item in the list. Try again");
+			rawInput = getInput();
+		}
+		
+		toDoService.remove(rawInput);
+	}
+	
+	public void markTaskComplete()
+	{
+		getList();
+		
+		int rawInput = getInput();
+		ArrayList<ToDoItem> toDo = toDoService.getAll();
+		
+		
+		while(rawInput > toDo.size() || rawInput < 0)
+		{
+			System.out.println("That is not an item in the list. Try again");
+			rawInput = getInput();
+		}
+		
+		toDoService.markComplete(rawInput);
+	}
+	
+	public void markTaskIncomplete()
+	{
+		getList();
+		
+		int rawInput = getInput();
+		ArrayList<ToDoItem> toDo = toDoService.getAll();
+		
+		
+		while(rawInput > toDo.size() || rawInput < 0)
+		{
+			System.out.println("That is not an item in the list. Try again");
+			rawInput = getInput();
+		}
+		
+		toDoService.markIncomplete(rawInput);
+	}
+	
+	public boolean isNumber(String userInput)
+	{
+		try
+		{
+			Integer.parseInt(userInput);
+			return true;
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+	}
+	
+	public int getStringInput()
+	{
+		boolean notValid = true;
+		int userInput = 0;
+		
+		System.out.println("Enter the title of a task: ");
+		do
+		{
+			String rawEntry = scan.nextLine();
+			try 
+            {
+				if(!isNumber(rawEntry))
+				{
+					if(toDoService.inList(rawEntry))
+					{
+						userInput = toDoService.getLineNumber(rawEntry);	
+					}
+				}
+				else
+				{
+					userInput = Integer.parseInt(rawEntry);
+					ArrayList<ToDoItem> toDo = toDoService.getAll();
+				
+					while(userInput > toDo.size() || userInput < 0)
+					{
+						System.out.println("That is not an item in the list. Try again");
+					}
+				}
+            } 
+            catch (Exception e) 
+            {
+                System.out.println("Invalid input.  Try again.");
+            }
+		}while (notValid);
+		
+		return userInput;
+	}
+	
 	public int getInput()
 	{
 		boolean notValid = true;
@@ -137,259 +436,6 @@ public class ToDoApp
 			counter++;
 		}
 	}
-
-	
-	public void getNewTask()
-	{
-		String newTask = null;
-		String getDate = null;
-		boolean inProgress = false;
-		String newUser = null;
-		String rawInput = null;
-		String getAnswer = null;
-		String newDescription = null;
-		
-		
-		System.out.println("Enter a new task: ");
-		newTask = scan.nextLine();
-		
-		while(newTask.length() < 1 || newTask.equals(" "))
-		{
-			System.out.println("Enter a new task: ");
-			newTask = scan.nextLine();
-		}
-		while(toDoService.inList(newTask) == true)
-		{
-			System.out.println("That task already exists, please enter a unique task name.");
-			newTask = scan.nextLine();
-		}
-		
-		
-		System.out.println("Enter the Due Date: ");
-		getDate = scan.nextLine();
-		while(!isDate(getDate))
-		{
-			System.out.println("Not a valid date");
-			getDate = scan.nextLine();
-		}
-		Date newDate = new Date ();
-		String pattern = "MM/dd/yyyy";
-	    SimpleDateFormat format = new SimpleDateFormat(pattern);
-		 
-	    try 
-	    {
-	      newDate = format.parse(getDate);
-	    } 
-	    catch (ParseException e) 
-	    {
-	      e.printStackTrace();
-	    }
-		
-	    System.out.println("Is this task in progress? (yes or no): ");
-		rawInput = scan.nextLine();
-		if (rawInput.equals("yes") || rawInput.equals("y"))
-		{
-			inProgress = true;
-		}
-		else 
-		{
-			inProgress = false;
-		}
-		
-		System.out.println("Who has to finish this task?: ");
-		newUser = scan.nextLine();
-		while(newUser.length() < 1 || newUser.equals(" "))
-		{
-			System.out.println("Enter a new task: ");
-			newUser = scan.nextLine();
-		}
-		
-		System.out.println("Did you want to add a description about this task? (yes or no");
-		getAnswer = scan.nextLine();
-		if(getAnswer == "no" || getAnswer == "n")
-		{
-			newDescription = null;
-		}
-		else if(getAnswer == "yes" || getAnswer == "y")
-		{
-			System.out.println("Please enter a description: ");
-			newDescription = scan.nextLine();
-		}
-		
-		toDoService.add(newTask, inProgress, newUser, newDate, newDescription);
-		
-		//scan.close();
-	}
-	
-	public boolean isDate(String input)
-	{
-		Date newDate = new Date ();
-		String pattern = "MM/dd/yyyy";
-	    SimpleDateFormat format = new SimpleDateFormat(pattern);
-		 try 
-		    {
-		      newDate = format.parse(input);
-		      return true;
-		    } 
-		    catch (ParseException e) 
-		    {
-		      //e.printStackTrace();
-		      return false;
-		    }
-	}
-	
-	public void selectUserTasks()
-	{
-		System.out.println("Input the name of a user you want to find the tasks for: ");
-		String getName = scan.nextLine();
-		
-		toDoService.getUserTask(getName);
-	}
-	
-	public void removeListItem()
-	{
-		getList();
-		
-		int rawInput = getInput();
-		ArrayList<ToDoItem> toDo = toDoService.getAll();
-		
-		
-		while(rawInput > toDo.size() || rawInput < 0)
-		{
-			System.out.println("That is not an item in the list. Try again");
-			rawInput = getInput();
-		}
-		
-		toDoService.remove(rawInput);
-	}
-	
-	public void updateTask()
-	{
-		getList();
-		int userEntry = getInput();
-		
-		String updateTask = null;
-		String getDate = null;
-		boolean inProgress = false;
-		String updateUser = null;
-		//String updateInput = null;
-		boolean isComplete = false;
-		String updateDescription = null;
-		String getAnswer = null;
-		
-		
-		System.out.println("Update the task: ");
-		updateTask = scan.nextLine();
-		
-		
-		System.out.println("Enter the new Due Date: ");
-		getDate = scan.nextLine();
-		Date updateDate = new Date ();
-		String pattern = "MM/dd/yyyy";
-	    SimpleDateFormat format = new SimpleDateFormat(pattern);
-	    try 
-	    {
-	      updateDate = format.parse(getDate);
-	    } 
-	    catch (ParseException e) 
-	    {
-	      e.printStackTrace();
-	    }
-		
-	    System.out.println("Is this task in progress? (yes or no): ");
-		String rawInput = scan.nextLine();
-		
-		if (rawInput == "yes"|| rawInput == "y")
-		{
-			inProgress = true;
-			
-		}
-		else if (rawInput.equals("no") || rawInput.equals("n"))
-		{
-			System.out.println("Is this task complete?");
-			String nextRawInput = scan.nextLine();
-			
-			if (nextRawInput.equals("yes") || nextRawInput.equals("y"))
-			{
-				isComplete = true;
-			}
-			else if (nextRawInput.equals("no") || nextRawInput.equals("n"))
-			{
-				isComplete = false;
-			}
-			
-			if (isComplete == false)
-			{
-				System.out.println("Who has to finish this task?: ");
-				updateUser = scan.nextLine();
-				while(updateUser.length() < 1 || updateUser.equals(" "))
-				{
-					System.out.println("Enter a new task: ");
-					updateUser = scan.nextLine();
-				}
-				
-			}
-			else if (isComplete == true)
-			{
-				System.out.println("Who finished this task?: ");
-				updateUser = scan.nextLine();
-				while(updateUser.length() < 1 || updateUser.equals(" "))
-				{
-					System.out.println("Enter a new task: ");
-					updateUser = scan.nextLine();
-				}
-			}
-			inProgress = false;
-		}
-		System.out.println("Did you want to add a description about this task? (yes or no");
-		getAnswer = scan.nextLine();
-		if(getAnswer == "no" || getAnswer == "n")
-		{
-			updateDescription = null;
-		}
-		else if(getAnswer == "yes" || getAnswer == "y")
-		{
-			System.out.println("Please enter a description: ");
-			updateDescription = scan.nextLine();
-		}
-		
-		toDoService.update(userEntry, updateTask, isComplete, inProgress, updateUser, updateDate, updateDescription);
-	}
-	
-	public void markTaskComplete()
-	{
-		getList();
-		
-		int rawInput = getInput();
-		ArrayList<ToDoItem> toDo = toDoService.getAll();
-		
-		
-		while(rawInput > toDo.size() || rawInput < 0)
-		{
-			System.out.println("That is not an item in the list. Try again");
-			rawInput = getInput();
-		}
-		
-		toDoService.markComplete(rawInput);
-	}
-	
-	public void markTaskIncomplete()
-	{
-		getList();
-		
-		int rawInput = getInput();
-		ArrayList<ToDoItem> toDo = toDoService.getAll();
-		
-		
-		while(rawInput > toDo.size() || rawInput < 0)
-		{
-			System.out.println("That is not an item in the list. Try again");
-			rawInput = getInput();
-		}
-		
-		toDoService.markIncomplete(rawInput);
-	}
-	
 
 	public void userChoice(int entryChoice)
 	{
