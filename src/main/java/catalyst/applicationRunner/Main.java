e0a64872e2850446f57862131819f63c5962bb64
unknown <arecore@PC10328.catalystsolves.com>
2015-10-19 13:27:17 -0700

    files made

--- src/main/java/catalyst/applicationRunner/Main.java
+++ src/main/java/catalyst/applicationRunner/Main.java
@@ -1,34 +1,42 @@
 package catalyst.applicationRunner;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import catalyst.application.ToDoServiceImpl;
 import catalyst.data.ToDoArrayList;
 import catalyst.presentation.ToDoApp;
 
-public class Main {
-
+@SpringBootApplication
+public class Main 
+{
+	
 	public static void main(String[] args) 
 	{
-		ToDoApp toDoApp = new ToDoApp();
-		ToDoArrayList toDoArrayList = new ToDoArrayList();
-		ToDoServiceImpl toDoService = new ToDoServiceImpl();
-		toDoApp.setToDoService(toDoService);
-		toDoService.setToDoData(toDoArrayList);
 		
-		System.out.println("Welcome to the To-Do List App!");
-		
-		int userInput = 0;
-		
-		do
-		{
-			 toDoApp.displayMenu();
-			 userInput = toDoApp.getInput();
-		     toDoApp.userChoice(userInput);	     
-		}while (userInput != 12);
-		
-		toDoApp.scan.close();
-		System.out.println("\nGoodbye!");
-		System.exit(0);	
+		SpringApplication.run(Main.class);
 	}
+//		ToDoApp toDoApp = new ToDoApp();
+//		ToDoArrayList toDoArrayList = new ToDoArrayList();
+//		ToDoServiceImpl toDoService = new ToDoServiceImpl();
+//		toDoApp.setToDoService(toDoService);
+//		toDoService.setToDoData(toDoArrayList);
+//		
+//		System.out.println("Welcome to the To-Do List App!");
+//		
+//		int userInput = 0;
+//		
+//		do
+//		{
+//			 toDoApp.displayMenu();
+//			 userInput = toDoApp.getInput();
+//		     toDoApp.userChoice(userInput);	     
+//		}while (userInput != 12);
+//		
+//		toDoApp.scan.close();
+//		System.out.println("\nGoodbye!");
+//		System.exit(0);	
+//	}
 
 }
 
